7ad6574f10922319c6a3cf287100ecf9662a296f
Stanislaw Gizinski <s.gizinski84@gmail.com>
2021-05-13 14:17:28 +0200

    new pipeline visualisation

--- visualise_pipeline.sh
+++ visualise_pipeline.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+SEARCH='strict digraph  {'
+REPLACE='strict digraph  {rankdir=LR;node  [style="rounded,filled,bold", shape=box, fixedsize=false, fontname="Arial"];edge  [style=bold, fontname="Arial" weight=2];'
+
+LINE_REPLACE_PATTERN="s/(.*) -> (.*);/\2 -> \1;/"
 
 dvc dag --dot > dot.txt
-sed -i -E "s/(.*) -> (.*);/\2 -> \1;/" dot.txt
+sed -i -E "$LINE_REPLACE_PATTERN" dot.txt
+sed -i "s/$SEARCH/$REPLACE/" dot.txt
 dot -Tpng dot.txt > pipeline.png
-rm dot.txt
 
 dvc dag --dot -o > dot.txt
-sed -i -E "s/(.*) -> (.*);/\2 -> \1;/" dot.txt
+sed -i -E "$LINE_REPLACE_PATTERN" dot.txt
+sed -i "s/$SEARCH/$REPLACE/" dot.txt
 dot -Tpng dot.txt > artifacts.png
+
 rm dot.txt
